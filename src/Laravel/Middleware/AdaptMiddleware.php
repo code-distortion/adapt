6477e96244f31f0b1b24829a72b3033ae9d1eae6
Tim Chandler <tim@code-distortion.net>
2021-01-22 20:49:49 +1100

    Tweaked the way the middleware re-sets the cookie

--- src/Laravel/Middleware/AdaptMiddleware.php
+++ src/Laravel/Middleware/AdaptMiddleware.php
@@ -33,10 +33,12 @@ class AdaptMiddleware
             return $next($request);
         }
 
-        $usedTempConfig = $this->useTemporaryConfig($request);
+        $cookieValue = $request->cookie(Settings::CONNECTIONS_COOKIE);
+        $usedTempConfig = $this->useTemporaryConfig($cookieValue);
+
         $response = $next($request);
         if ($usedTempConfig) {
-            $this->reSetCookie($request, $response);
+            $this->reSetCookie($response, $cookieValue);
         }
         return $response;
     }
@@ -44,13 +46,13 @@ class AdaptMiddleware
     /**
      * Read the Adapt cookie, and if present, load the temporary config file it points to.
      *
-     * @param Request $request The request object.
+     * @param string|null $cookieValue The cookie value.
      * @return boolean
      * @throws AdaptBrowserTestException When there was a problem loading the temporary config.
      */
-    private function useTemporaryConfig(Request $request): bool
+    private function useTemporaryConfig(?string $cookieValue): bool
     {
-        $tempCachePath = $this->getTempCachePath($request);
+        $tempCachePath = $this->getTempCachePath($cookieValue);
         if (!$tempCachePath) {
             return false;
         }
@@ -77,12 +79,11 @@ class AdaptMiddleware
     /**
      * Pick the temporary cache-path from the Adapt cookie.
      *
-     * @param Request $request The request object.
+     * @param string|null $cookieValue The cookie value.
      * @return string|null
      */
-    private function getTempCachePath(Request $request): ?string
+    private function getTempCachePath(?string $cookieValue): ?string
     {
-        $cookieValue = $request->cookie(Settings::CONNECTIONS_COOKIE);
         if (!is_string($cookieValue)) {
             return null;
         }
@@ -121,21 +122,20 @@ class AdaptMiddleware
     /**
      * Add the database config settings to the cookie again - to help it stay when the user logs out.
      *
-     * @param Request        $request  The request object.
-     * @param Response|mixed $response The response object.
+     * @param Response|mixed $response    The response object.
+     * @param string|null    $cookieValue The cookie value.
      * @return void
      */
-    private function reSetCookie(Request $request, $response): void
+    private function reSetCookie($response, ?string $cookieValue): void
     {
         if (!($response instanceof Response)) {
             return;
         }
 
-        $cookieValue = $request->cookie(Settings::CONNECTIONS_COOKIE);
         if ((!is_string($cookieValue)) || (!mb_strlen($cookieValue))) {
             return;
         }
 
-        $response->cookie(Settings::CONNECTIONS_COOKIE, $cookieValue, null, '/', $request->getHost(), false, false);
+        $response->cookie(Settings::CONNECTIONS_COOKIE, $cookieValue, null, '/', null, false, false);
     }
 }
